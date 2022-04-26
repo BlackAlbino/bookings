8577d0998cb45c13732338804b25bc4fcfd43772
tgoldboom <tgoldboom@googlemail.com>
2022-04-26 19:08:43 +0200

    Initial commit

---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,48 @@
+package handlers
+
+import (
+	"github.com/BlackAlbino/bookings/pkg/config"
+	"github.com/BlackAlbino/bookings/pkg/models"
+	"github.com/BlackAlbino/bookings/pkg/render"
+	"net/http"
+)
+
+// Repo used by the handlers
+var Repo *Repository
+
+//Repository is the repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+//NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+//NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+// About is the handler for the about page
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again."
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
+
+	render.RenderTemplate(w, "about.page.templ", &models.TemplateData{
+		StringMap: stringMap,
+	})
+
+}
+
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+	render.RenderTemplate(w, "home.page.templ", &models.TemplateData{})
+}
